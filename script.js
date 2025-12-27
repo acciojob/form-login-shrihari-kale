b435e5847ef44537bb44f2be59f364bc7ae8c7f3
Shrihari Kale <163184413+shrihari-kale@users.noreply.github.com>
2025-12-27 21:07:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+    let firstName = document.forms[0].fname.value.trim();
+    let lastName = document.forms[0].lname.value.trim();
+
+    let fullName = firstName + " " + lastName;
+
+    alert(fullName.trim());
+	}
 
 }
